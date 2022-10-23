90102b58e61008153e6fd7c5719bc71a83ca09b3
dextermayhewjd <78476926+dextermayhewjd@users.noreply.github.com>
2022-10-23 17:36:20 +0100

    first component added
    written in the same class and aaded like an element

--- src/App.js
+++ src/App.js
@@ -6,22 +6,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
+       My hello World
+       <FirstCompoment></FirstCompoment>
       </div>
     );
   }
 }
+
+class FirstCompoment extends Component {
+  render() {
+    return (
+      <div className="firstComponent">
+       FirstCompoment
+      </div>
+    );
+  }
+}
+
 export default App;
