3a2b8f9be917964324327dec813c87ac00e704a9
dextermayhewjd <78476926+dextermayhewjd@users.noreply.github.com>
2022-10-25 00:05:32 +0100

    counter could be incremented in the same time

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import FirstComponent from './components/learning-examples/FirstComponent'
 import SecondComponent from './components/learning-examples/SecondComponent.';
 import ThirdComponent from './components/learning-examples/ThirdComponent';
 import Counter from './components/counter/Counter';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,11 +1,41 @@
-import React from "react"
+import React, { Component } from "react"
+import './Counter.css'
+class Counter extends Component{
+  
+  // define the initial state in a constructor 
+  //state => counter 0
+  
+  constructor(){
+    super();
+   
+    this.state = {
+      counter : 0,
+      secondCounter : 100
+    }
 
-function Counter(){
+    this.increment = this.increment.bind(this)
+  }
+
+
+  render(){
   return(
     <div className="counter">
-      Counter
-    </div>
-  )
-}
+      {/* <button onclick={someJSMethodCall()}>+1</button> */}
+      <button onClick={this.increment}>+1</button>
+      <span className="count">{this.state.counter}</span>
+      <span className="count">{this.state.secondCounter}</span>
 
+    </div>
+    )
+  }
+
+ increment(){ // update the state
+              // counter ++
+
+  this.setState({
+    counter: this.state.counter+1 ,
+    secondCounter:  this.state.secondCounter + 1
+ });
+ }
+}
 export default Counter
