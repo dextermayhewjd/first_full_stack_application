c7ffe4b49ac2c8f7fd10ada5489cd35825d38a58
dextermayhewjd <yv19140@bristol.ac.uk>
2022-11-01 13:30:22 +0000

    learn how to create a controlled component
    using handler to change the state using bind (this) and event (using to setState).  for the html part need to use onChange

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -10,11 +10,38 @@ class TodoApp extends Component{
     }
 }
 class LoginComponent extends Component{
+    // it is a controlled components because the state is insede the react component
+    constructor(props){
+        super(props)
+        this.state={
+            username:'DexterDing',
+            password:''
+        
+        }
+        this.handlerUsernameChange = this.handlerUsernameChange.bind(this)
+        this.handlerPasswordChange = this.handlerPasswordChange.bind(this)
+    }
+    handlerUsernameChange(event){
+        console.log(event.target.value)
+        this.setState(
+            {
+            username:event.target.value
+             }
+        )
+    }
+    handlerPasswordChange(event){
+        console.log(event.target.value)
+        this.setState(
+            {
+            password:event.target.value
+             }
+        )
+    }
     render(){
         return(
             <div>
-            User Name : <input type="text" name="username"/>
-            Password: <input type="text" name="password"/>
+            User Name : <input type="text" name="username" value={this.state.username}onChange={this.handlerUsernameChange}/>
+            Password: <input type="text" name="password"value={this.state.password}onChange={this.handlerPasswordChange}/>
             <button>Login</button>
             </div>
             )
