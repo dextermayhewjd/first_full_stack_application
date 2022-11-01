92ebd58c9aa8148fead6d1ec2cf1357942fb5a91
dextermayhewjd <yv19140@bristol.ac.uk>
2022-11-01 14:57:30 +0000

    implement the login component logic
    write a function outside the class and the return value is html
    the logic function that written in Character
    three parts is separate
    first is the login clicked event
    second is the javascript in the render <div> part
    third is the logic function that written outside the class and the return value is <div> or null

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -16,18 +16,22 @@ class LoginComponent extends Component{
         super(props)
         this.state={
             username:'DexterDing',
-            password:''
+            password:'',
+            hasLoginFailed:false,
+            showSuccessMessage:false
         
         }
         // this.handlerUsernameChange = this.handlerUsernameChange.bind(this)
         // this.handlerPasswordChange = this.handlerPasswordChange.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        this.loginClicked = this.loginClicked.bind(this)
     }
 
     handleChange(event){
         console.log(event.target.name);
         this.setState(
             {
+                //key point for a general handler 
                 [event.target.name]
                     :event.target.value
             }
@@ -49,14 +53,55 @@ class LoginComponent extends Component{
     //          }
     //     )
     // }
+    loginClicked(){
+        //dexterding,Jam198522
+        // this in class function is a function to determine and change state 
+        // based on the information that passed in 
+        if(this.state.username==='dexterding'&&this.state.password==='Jam198522')
+        {
+            console.log('Successful')
+            this.setState({showSuccessMessage:true})
+            this.setState({hasLoginFailed:false})
+
+        }
+        else{
+            this.setState({showSuccessMessage:false})
+            this.setState({hasLoginFailed:true})
+            console.log('Failed')
+
+        // console.log(this.state)
+        }
+    }
     render(){
         return(
             <div>
+                {/* here are two function called with state's true or false boolen passed in 
+                the ShowInvalidCredential and howLoginSuccessMessage are function written 
+                ouside the task */}
+                <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/>
+                <ShowLoginSuccessMessage showSuccessMessage ={this.state.showSuccessMessage}/>
             User Name : <input type="text" name="username" value={this.state.username}onChange={this.handleChange}/>
             Password: <input type="text" name="password"value={this.state.password}onChange={this.handleChange}/>
-            <button>Login</button>
+            <button onClick={this.loginClicked}>Login</button>
             </div>
             )
-    }w
+    }
+
+    
+
+}
+
+function ShowInvalidCredentials(props) {
+    if(props.hasLoginFailed){
+        return<div>Invaild Credentials</div>
+        // the return value could be html script
+    }
+    return null
+}
+function ShowLoginSuccessMessage(props) {
+    if(props.showSuccessMessage){
+        return<div>Login Sucessful</div>
+    }
+    return null
 }
 export default TodoApp
