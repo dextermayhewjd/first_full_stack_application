9ff872f92b5325c70c95df1d9c0e4ee4a1227438
dextermayhewjd <yv19140@bristol.ac.uk>
2022-11-03 19:37:32 +0000

    later would be converted into axious related connection things

--- front-end/src/components/todo/WelcomeComponent.jsx
+++ front-end/src/components/todo/WelcomeComponent.jsx
@@ -2,16 +2,33 @@ import React,{Component} from "react";
 import {  Link, } from 'react-router-dom'
 
 class WelcomeComponent extends Component {
+
+    constructor(props){
+        super(props)
+        this.retrieveEwlcomeMessage = this.retrieveEwlcomeMessage.bind(this)
+    }
+
     render() {
         return (
             <>
             <h1>Welcome!</h1>
             <div className="container">
                 {/* Welcome {this.props.params.name}. You can manage your todos <a href="/todos">here</a>.  */}
-                Welcome {this.props.params.name}. You can manage your todos <Link to="/todos">here</Link>. 
+                    Welcome {this.props.params.name}. You can manage your todos <Link to="/todos">here</Link>. 
+            </div>
+            <div className="container">
+                click here to get a customized welcome message
+                You can manage your todos <Link to="/todos">here</Link>
+            <button onClick={this.retrieveEwlcomeMessage}
+            className="btn btn-success">Get welcome message</button>
             </div>
             </>
         )        
     }
+        retrieveEwlcomeMessage(){
+            console.log('retrieve clicked')
+        }
+
+
 }
 export default WelcomeComponent
