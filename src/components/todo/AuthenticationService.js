5c21ab178c2fbf34b978ba2bb691541e9c566028
dextermayhewjd <yv19140@bristol.ac.uk>
2022-11-03 11:27:48 +0000

    add the logout with session removed
    changed the logout button
    add the removesession in the authenticatonService

--- src/components/todo/AuthenticationService.js
+++ src/components/todo/AuthenticationService.js
@@ -1,6 +1,11 @@
 class AuthenticationService{
     registerSuccessfullLogin(username,password){
+        //data stored in localStorage has no expiration time 
+        //data stored in sessionStorage gets cleared when the page seesion ends
         sessionStorage.setItem('authenticatedUser',username);
+    }
+    logout(){
+        sessionStorage.removeItem('authenticatedUser')
     }
 }
 export default new AuthenticationService()
