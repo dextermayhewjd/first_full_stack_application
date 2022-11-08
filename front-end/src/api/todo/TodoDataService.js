771c4c55f2422eca497584f503fe9f4b4b098707
dextermayhewjd <78476926+dextermayhewjd@users.noreply.github.com>
2022-11-08 00:18:48 +0000

    user constants to replace URL and Token
    1. define a constant.js file
    when the constant is need just import and use $ { } ` `
    2. in class variable just use
    export const USER_NAME_SESSION_ATTRIBUTE_NAME = 'authenticatedUSer'

--- front-end/src/api/todo/TodoDataService.js
+++ front-end/src/api/todo/TodoDataService.js
@@ -1,20 +1,21 @@
 import axios from "axios"
+import { BACK_END_API_URL } from "../../Constants";
 class TodoDataService{
     retrieveAllTodos(name){
-    return axios.get(`http://localhost:8080/users/${name}/todos`);
+    return axios.get(`${BACK_END_API_URL}/users/${name}/todos`);
     }
 
     retrieveTodo(name,id){
-        return axios.get(`http://localhost:8080/users/${name}/todos/${id}`);
+        return axios.get(`${BACK_END_API_URL}/users/${name}/todos/${id}`);
         }
     deleteTodo(name,id){
-        return axios.delete(`http://localhost:8080/users/${name}/todos/${id}`);
+        return axios.delete(`${BACK_END_API_URL}/users/${name}/todos/${id}`);
     }
     updateTodo(name,id,todo){
-        return axios.put(`http://localhost:8080/users/${name}/todos/${id}`,todo);
+        return axios.put(`${BACK_END_API_URL}/users/${name}/todos/${id}`,todo);
     }
     createTodo(name,todo){
-        return axios.post(`http://localhost:8080/users/${name}/todos/`,todo);
+        return axios.post(`${BACK_END_API_URL}/users/${name}/todos/`,todo);
     }
 }
 // when it is a service need to add () in the end
